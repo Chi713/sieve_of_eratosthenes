35f14915f6b4c1325d4252caffefcd2a0436c70b
Chris Hoskin <>
2021-04-26 15:10:42 +0000

    Created Boolean vector for primes

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
 //use math::round;
 
 fn main() {
-    let range =9 + 1;
+    let range =9;
     let max_check = (range as f64).sqrt().trunc() as usize;
-    let mut list = vec![false; range-1];
+    let mut list = vec![false; range -1];
+    //let mut final_list =Vec::new();
 
     println!("max_check: {}", max_check);
 
-    for i in 2..max_check {
+    for i in 0..(max_check-1) {
         
         println!("i: {}\n", i);
         
-        if list[i-2] == false { 
-            for j in 2..range {
+        if list[i] == false { 
+            for j in i..(range-1) {
                 println!("j: {}",j);
-                println!("j mod(i): {}", j% i);
-                println!("list at index:{}", list[j-2]);
-
-                list[j-2] = list[j-2] ^ (j % i == 0);
-
-                println!("list at index:{}", list[j-2]);
+                println!("j mod(i): {}", ((j+2) % (i+2)));
+                println!("list at index:{}", list[j]);
+                if list[j] == false {
+                  list[j] = ((((j+2) % (i+2)) == 0) & (j!= i));
+                }
+                println!("list at index:{}", list[j]);
             }
         }
-        //println!("{:?}", list);
+        println!("{:?}", list);
         //println!("{}", i);
     }
-    println!("{:?}", list);
+    println!("final: {:?}", list);
+   /* for count in list.iter() {
+      final_list[count] = (2+count); 
+    } */
 }
