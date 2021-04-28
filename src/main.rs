3c1f5a7cfb2a45a67ab8a91564a795cbd721fe58
Chi713 <chris.hoskin2355@gmail.com>
2021-04-28 07:36:37 -0400

    Refactored sieving into seperate function call.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,54 @@
 //use math::round;
 use std::time::Instant;
+use std::io;
 
 fn main() {
+    // Prepare input for sieving function
+    println!("enter value");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("failed to read line");
+    //println!("{:?}", guess);
+    let len = guess.trim_end_matches(&['\r', '\n'][..]).len();
+    guess.truncate(len);
+
+    let range: usize = guess.parse().unwrap();
+    
+    // Start function timer
     let start = Instant::now();
-    let range = 1000000;
+
+    //let range = 1000000;
+
+    let result = sieve(range);
+    // Stop function timer
+    let duration = start.elapsed();
+    println!("value: {}", result[result.len()-1]);
+    println!("binary: {:#034b}", result[result.len()-1]);
+    println!("Time Elapsed: {:?}", duration);
+}
+
+
+fn sieve(range: usize) -> Vec<usize> {
     let max_check = (range as f64).sqrt().trunc() as usize;
     let mut list = vec![false; range -1];
     let mut final_list: Vec<usize> = Vec::new();
 
-    //println!("max_check: {}", max_check);
-
     for i in 0..(max_check-1) {
-        
-        //println!("i: {}\n", i);
         
         if list[i] == false { 
             for j in i..(range-1) {
-                //println!("j: {}",j);
-                //println!("j mod(i): {}", ((j+2) % (i+2)));
-                //println!("list at index:{}", list[j]);
                 if list[j] == false {
                   list[j] = ((j+2) % (i+2) == 0) & (j!= i);
                 }
-                //println!("list at index:{}", list[j]);
             }
         }
-        //println!("{:?}", list);
-        //println!("{}", i);
     }
-    //println!("final: {:?}", list);
-   for (pos, value) in list.iter().enumerate() {
-      //println!("{:?}", pos);
-      if !*value {
-        final_list.push(2+pos);
+    let mut pos: usize = 2;
+   for value in list {
+      if !value {
+        final_list.push(pos);
       }
-      //final_list[pos] = (2+pos) * (*value==false) as usize; 
+      pos += 1;
     } 
-
-    let duration = start.elapsed();
-    println!("{}", final_list[final_list.len()-1]);
-    println!("Time Elapsed: {:?}", duration);
+    // Returns list
+    final_list
 }
